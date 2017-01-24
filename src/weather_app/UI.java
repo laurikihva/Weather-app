3a7059b4274b7fb5f8b26b1ecad5880218b07177
Lauri <Lauri>
2017-01-24 18:19:40 +0200

    added degree, wind and other labels

--- src/weather_app/UI.java
+++ src/weather_app/UI.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
@@ -21,13 +23,13 @@ public class UI extends Application {
     public void start(Stage primary) {
         primary.setTitle("Weather Application");
         GridPane location = new GridPane();
-        location.setAlignment(Pos.CENTER);
+        location.setAlignment(Pos.TOP_LEFT);
         location.setHgap(10);
         location.setVgap(10);
-        location.setPadding(new Insets(25, 25, 25, 25));
+        location.setPadding(new Insets(15, 25, 25, 25));
 
         /* Creating the scene box with measurements */
-        Scene box = new Scene(location, 325, 275);
+        Scene box = new Scene(location, 350, 275);
         primary.setScene(box);
 
         /* Setting up action title */
@@ -35,8 +37,28 @@ public class UI extends Application {
         topTitle.setFont(Font.font("Calibri", FontWeight.NORMAL, 15));
         location.add(topTitle, 1, 0, 2, 1);
 
+        /* Text field for the Country and City */
         TextField userChoice = new TextField();
         location.add(userChoice, 1, 1);
+
+        /* The button to submit the choice */
+        Button btn = new Button("Submit");
+        location.add(btn, 2, 1);
+
+        /* Label for the city */
+        Label city = new Label("Tallinn:");
+        city.setFont(Font.font("Calibri", FontWeight.NORMAL, 25));
+        location.add(city, 1, 2);
+
+        /* Main degree position */
+        Label degree = new Label("20°C");
+        degree.setFont(Font.font("Calibri", FontWeight.NORMAL, 30));
+        location.add(degree, 1, 3);
+
+        /* Wind speed */
+        Label wind = new Label("1,3 mps");
+        wind.setFont(Font.font("Calibri", FontWeight.NORMAL, 30));
+        location.add(wind, 2, 3);
 
         primary.show();
     }
